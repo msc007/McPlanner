4da70c73658b35fdb55272f6d3bc11936226c2e6
msc007 <alstjr5569@gmail.com>
2019-01-17 23:45:20 -0800

    routing link done; background img added; index.css modified

--- src/components/layout/SignedInLink.js
+++ src/components/layout/SignedInLink.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 export default function SignedInLinks() {
   return (
     <ul className="right">
-			<li><NavLink to="/">New Proeject</NavLink></li>
+			<li><NavLink to="/create">New Proeject</NavLink></li>
 			<li><NavLink to="/">Log Out</NavLink></li>
 			<li><NavLink to="/" className="btn btn-floating pink lighten-1">MC</NavLink></li>
 		</ul>
