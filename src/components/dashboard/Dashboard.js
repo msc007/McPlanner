21d51161ee78ce9839cdc944cd6cfee37471cfc9
msc007 <alstjr5569@gmail.com>
2019-01-18 20:49:02 -0800

    dashboard projects now uses real data from firestore

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -29,7 +29,7 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
 	console.log(state);
 	return {
-		projects: state.project.projects
+		projects: state.firestore.ordered.projects
 	}
 }
 
