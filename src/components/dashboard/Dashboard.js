a74415c63364359aafeca81280b7b37460e75593
msc007 <alstjr5569@gmail.com>
2019-01-21 22:06:52 -0800

    frontend for notification now working

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,7 @@ import { Redirect } from 'react-router-dom'
 class Dashboard extends Component {
   render() {
 		//console.log(this.props);
-		const { projects, auth } = this.props;
+		const { projects, auth, notifications } = this.props;
 		
 		if(!auth.uid) 
 			return <Redirect to='/signin' />
@@ -21,7 +21,7 @@ class Dashboard extends Component {
 						<ProjectList projects={projects}/>
 					</div>
 					<div className="col s12 m5 offset-m1">
-						<Notifications />
+						<Notifications notifications={notifications}/>
 					</div>
 				</div> 
 			</div>
@@ -34,7 +34,8 @@ const mapStateToProps = (state) => {
 	console.log(state);
 	return {
 		projects: state.firestore.ordered.projects,
-		auth: state.firebase.auth
+		auth: state.firebase.auth,
+		notifications: state.firestore.ordered.notifications
 	}
 }
 
@@ -44,6 +45,7 @@ export default compose(
 	connect(mapStateToProps),
 	//connect to firestore projects collection
 	firestoreConnect([
-		{collection: 'projects'}
+		{collection: 'projects'},
+		{collection: 'notifications', limit: 3}
 	])
 )(Dashboard);
